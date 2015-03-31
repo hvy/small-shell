0eea8788e703f95b7b40a1035ea1e4160ca351f6
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-03-31 18:31:39 +0200

    added exit command

--- src/shell.c
+++ src/shell.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 
 #define MAX_INPUT_CHARS       80
 #define MAX_DIRECTORY_CHARS   100
+#define COMMAND_EXIT          "exit\n"
+
 
 int main(int argc, char *argv[], char *envp[]) {
 
@@ -17,10 +20,14 @@ int main(int argc, char *argv[], char *envp[]) {
   char directory[MAX_DIRECTORY_CHARS];
 
   while(1) {
-
     getcwd(directory, MAX_DIRECTORY_CHARS);
     printf("%s@%s->", getlogin(), directory);
     getline(&input, &maxInputChars, stdin);    
+  
+    if(strcmp(input, COMMAND_EXIT) == 0) {
+      exit(0);    
+    }
+  
   }
 
 	return 0;
