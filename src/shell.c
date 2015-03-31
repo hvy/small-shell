874decffd529c8cc066a597a74e0c264509a2c1d
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-03-31 18:25:58 +0200

    prints cwd and waits for input

--- src/shell.c
+++ src/shell.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main(int argc, char *argv[]) {
+#define MAX_INPUT_CHARS       80
+#define MAX_DIRECTORY_CHARS   100
 
-	printf("Hello World\n");
+int main(int argc, char *argv[], char *envp[]) {
+
+  //execve("/bin/ls", argv, envp);
+
+  size_t maxInputChars = MAX_INPUT_CHARS;
+  char* input = (char*) malloc(sizeof(char) * MAX_INPUT_CHARS);
+
+  char directory[MAX_DIRECTORY_CHARS];
+
+  while(1) {
+
+    getcwd(directory, MAX_DIRECTORY_CHARS);
+    printf("%s@%s->", getlogin(), directory);
+    getline(&input, &maxInputChars, stdin);    
+  }
 
 	return 0;
 }
